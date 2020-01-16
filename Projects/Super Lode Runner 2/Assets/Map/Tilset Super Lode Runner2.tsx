de5ab502da2d70d998bddc8a03e585009d18722b
Jonathan <jonathanfrt97480@gmail.com>
2020-01-16 23:08:15 +0400

    Add assets
    Addition of active ho project.
    active add full spritshett, sound effects and game music and font

---
+++ Projects/Super Lode Runner 2/Assets/Map/Tilset Super Lode Runner2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Tilset Super Lode Runner2" tilewidth="16" tileheight="17" tilecount="80" columns="10">
+ <image source="../Image/Tilset Super Lode Runner2.png" width="160" height="136"/>
+</tileset>
